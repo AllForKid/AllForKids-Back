ae82f4a106029bc063600a43cfdda03e55dcb0cb
kchaeeun <oceanball38@naver.com>
2023-09-05 12:42:13 +0900

    [FEAT] 프로젝트 생성

---
+++ src/test/java/AFK/AFK/AfkApplicationTests.java
@@ -0,0 +1,13 @@
+package AFK.AFK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
