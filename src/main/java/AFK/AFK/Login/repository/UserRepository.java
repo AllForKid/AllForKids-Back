cb615fb17ef69bf83bd4ebb18ae12b4df2760818
kchaeeun <oceanball38@naver.com>
2023-09-25 20:09:14 +0900

    [MOD] User 겹치는 파일 삭제

--- src/main/java/AFK/AFK/Login/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package AFK.AFK.Login.repository;
-
-import AFK.AFK.Login.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
